4505387ac75ec6622f25084968c9063b8807bd0c
cliuff <35420834+cliuff@users.noreply.github.com>
2021-08-09 14:26:19 +0800

    Add Gradle project configuration

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 Clifford Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.code.findbugs:jsr305:3.0.2")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.58")
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.58")
+    testImplementation("junit:junit:4.13.2")
+}
+
+group = "net.dongliu"
+version = "2.6.10-cliuff"
+description = "apk-parser"
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
